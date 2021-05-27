2887751e5a7d1507ef3dece17b13ebef7e1a50d5
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2021-05-28 01:36:54 +0500

    calculateIsVerified

--- src/pages/_/posts/VerifyPost.js
+++ src/pages/_/posts/VerifyPost.js
@@ -10,17 +10,18 @@ import { FormGroup, Input, InputGroupAddon, Label, Modal, ModalBody, ModalFooter
 import { toggleDeleteProductDisclosure } from "store/routes/products/actions";
 
 function VerifyPost({ isOpen, toggle, post }) {
-    console.log(post.verified);
+    const calculateIsVerified = useCallback(() => {
+        if (!post.verified) return false;
+        return Object.values(post.verified).filter(Boolean).length >= 2;
+    }, [post]);
+
     return (
         <Modal isOpen={isOpen} toggle={toggle} centered>
             <ModalHeader>Verify Post</ModalHeader>
             <ModalBody>
                 <FormGroup check inline>
                     <Label check>
-                        <Input
-                            type="checkbox"
-                            //defaultChecked={Object.values(post.verified).filter(Boolean).length >= 2}
-                        />{" "}
+                        <Input type="checkbox" defaultChecked={calculateIsVerified()} />
                         Verify post
                     </Label>
                 </FormGroup>
